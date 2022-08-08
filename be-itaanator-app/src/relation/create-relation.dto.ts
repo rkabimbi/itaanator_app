d5ef7c77fd2c7f0aa058ab0ffec7367b1eb8abb7
Rudy Kabimbi Ngoy <rudykabimbingoy@Mini-de-Rudy.lan>
2022-08-08 17:41:15 +0200

    fetch fonctionne nickel

--- be-itaanator-app/src/relation/create-relation.dto.ts
+++ be-itaanator-app/src/relation/create-relation.dto.ts
@@ -1,10 +1,11 @@
 import { Contract } from "src/contract/contract.entity";
+import { VATLiability } from "./relation.entity";
 
 //Dto impose une syntaxe/type entre front-end et back-end (principalement à destination des services donc)
 export class CreateRelationDto{
     readonly id: number;
     //readonly timestamp: Date;
-    readonly VATLiability: string;
+    readonly VATLiability: VATLiability;
     readonly taxDebt: boolean;
     readonly finDebt: boolean;
     readonly contracts: Contract[];
--- be-itaanator-app/src/relation/relation.entity.ts
+++ be-itaanator-app/src/relation/relation.entity.ts
@@ -3,6 +3,16 @@ import { Contract } from "src/contract/contract.entity";
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique, PrimaryColumn, ManyToMany, JoinTable } from "typeorm";
 
 
+export enum VATLiability{
+    MENSUEL= "Mensuel",
+    TRIMESTRIEL ="Trimestriel",
+    NA="Non assujetti",
+    FRANCHISE="Franchisé",
+    EXEMPTE="Exempté"
+
+}
+
+
 @Entity()
 @Unique(["id"])
 export class Relation extends BaseEntity{
@@ -14,9 +24,9 @@ export class Relation extends BaseEntity{
     //@Column()
     //timestamp: Date;
     
-    @Column({default:"MENSUEL"})
-    @IsString()
-    VATLiability: string;
+    
+    @Column({ type:'enum',enum:VATLiability, default:VATLiability.TRIMESTRIEL})
+    VATLiability: VATLiability;
 
     @Column({default:false})
     taxDebt: boolean;
