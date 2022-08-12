aa5239a4491870a56abd4fa6813c8039c8786888
Rudy Kabimbi Ngoy <rudykabimbingoy@Mini-de-Rudy.lan>
2022-08-12 21:49:31 +0200

    le tableau est full ok

--- fe-itaanator-app-ts/src/components/list/Table.js
+++ fe-itaanator-app-ts/src/components/list/Table.js
@@ -5,88 +5,19 @@ import { DataGrid } from "@mui/x-data-grid";
 import { width } from "@mui/system";
 import { useState } from "react";
 
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
-
-const rows = [
-  { id: 1, confrere: "Snow", digitalInvoice: false, invoiceAnnexe: true, invoicingMethod: true, comment:"blablabla",budget:5200 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
 
 
 //display all the received arrays in a UI MATERIAL list
 export function Table(props) {
   
-  //const [columns2, setColumns] = useState();
-  /*
-  useEffect(() => {
-    
-  
-      let result = [];
-  
-
-      let columnDescription = {
-        field: "",
-        headerName: "",
-        width: "",
-      };
-
-      for (let title in props.jsonKeys) {
-        columnDescription.field = title;
-        columnDescription.headerName = title;
-        columnDescription.width = 70;
-        let copy = { ...columnDescription }; //deepcoy
-        result.push(copy);
-        console.log("foreach")
-      }
-
-       setColumns(result);
-      console.log("col2")
-      console.log(columns2)
-    }
-
-  , []);
-  */
-
-  console.log("dans list");
-  console.log("dans list - props : \n",props);
-  console.log("dans list - props.contracts : \n",props.contracts);
-  console.log("dans list - props.jsonkeys : \n",props.jsonKeys);
-  console.log("dans list - props.columnsInfos : \n",props.columnsInfos);
 
 
-
-if(props.columnsInfos){
+if(props.columnsInfos && props.contracts){
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div className='tabContract' style={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
+        rows={props.contracts}
         columns={props.columnsInfos}
         pageSize={5}
         rowsPerPageOptions={[5]}
@@ -96,17 +27,7 @@ if(props.columnsInfos){
     </div>
   );
 
-} 
+}
 }
 
-/*
-{props.contracts &&
-  props.contracts.map((contract) => (
-    <div>
-      <h3> budget : {contract.budget}</h3>
-      <h3> commentaire : {contract.comment}</h3>
-      <h3> invoice method: {contract.invoicingMethod}</h3>
-    </div>
-  ))}
 
-  */
--- fe-itaanator-app-ts/src/pages/contract/ContractPage.js
+++ fe-itaanator-app-ts/src/pages/contract/ContractPage.js
@@ -7,48 +7,6 @@ export function ContractPage() {
   const [jsonKeys, setJsonKeys] = useState();
   const [columnsInfos, setColumnsInfos] = useState();
 
-  /*
-  useEffect(() => {
-    console.log("fetch sur contract");
-    //i'm fetching here to make my ListComponent reussable after
-  
-    fetch("http://localhost:3001/contract")
-      .then((response) => response.json())
-      .then((data) => {
-        setContracts(data);
-        setJsonKeys(Object.keys(data[0])); //allow to get the keys of JSON. I need this to make Table components reusable
-        console.log("contracts: ", contracts);
-        return jsonKeys;
-      })
-      .then((res) => {
-        console.log("then 3")
-        let result = [];
-
-        let columnDescription = {
-          field: "",
-          headerName: "",
-          width: "",
-        };
-        console.log("then 3 - for each")
-        for (let columnInfo in res) {
-          console.log("then 3 - for each - num...")
-          columnDescription.field = columnInfo;
-          columnDescription.headerName = columnInfo;
-          columnDescription.width = 70;
-          let copy = { ...columnDescription }; //deepcoy
-          result.push(copy);
-        }
-        console.log("then 3 - set state")
-        setColumnsInfos(result);
-      })
-
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
-
-  */
-
   useEffect(() => {
     console.log("fetch sur contract");
     //i'm fetching here to make my ListComponent reussable after
@@ -57,13 +15,11 @@ export function ContractPage() {
       .then((response) => response.json())
       .then((data) => {
         console.table(data);
-        //setContractsAndJsonKeys(data)
-        return fcttest2(data);
+        return setTheStateContracts(data);
       })
       .then((data) => {
         console.table(data);
-        //setContractsAndJsonKeys(data)
-        return fcttest(data);
+        return getJsonKeys(data);
       })
       .then((res) => {
         console.log("then 3");
@@ -75,91 +31,35 @@ export function ContractPage() {
           width: "",
         };
         console.log("then 3 - for each");
-        console.log("keys :",res)
+        console.log("keys :", res);
         for (let columnInfo of res) {
           console.log("then 3 - for each - num...");
           columnDescription.field = columnInfo;
           columnDescription.headerName = columnInfo;
-          columnDescription.width = 70;
+          columnDescription.width = 180;
           let copy = { ...columnDescription }; //deepcoy
           result.push(copy);
         }
         console.log("then 3 - set state");
         setColumnsInfos(result);
-        console.log("putain de result:",result)
+        console.log("putain de result:", result);
       });
   }, []);
 
-  const fcttest = (data) => {
+  const getJsonKeys = (data) => {
     return new Promise((resolve) => {
       console.log("data a affiche", data);
       resolve(Object.keys(data[0]));
     });
   };
 
-  const fcttest2 = (data) => {
+  const setTheStateContracts = (data) => {
     return new Promise((resolve) => {
       console.log("contracts a affiche", data);
-      setContracts(data)
+      setContracts(data);
       resolve(data);
     });
   };
-
-  /*
-//déclarer
-  const setContractsAndJsonKeys = (args) =>{
-    return new Promise( (resolve) => {
-      console.log("resolve1 :\n" ,  "args :", args)//ca doit tjrs s'afficher vu qu'il a attendu les args avant de venir ici
-      let dicto={
-        id:7,
-        confrere: "Snow", 
-        digitalInvoice: false, 
-        invoiceAnnexe: true, 
-        invoicingMethod: true, 
-        comment:"blablabla",
-        budget:5200
-      }
-      setColumnsInfos(dicto)
-      console.log("column infos :",columnsInfos)
-    })
-  }
-
-
-  const setDicto = (args) =>{
-    return new Promise( (resolve) => {
-      console.log("resolve2 :\n" ,  "args :", args)
-      setColumnsInfos(args)
-      resolve(columnsInfos)
-     
-    })
-  }
-
-
-*/
-
-  /*
-  const setTitlesFunction = function setTitlesFunc () { 
-      let result = []
-
-      let titleDescription = {
-        field: "",
-        headerName: "",
-        width: "",
-      };
-
-      for (let title in jsonKeys) {
-        titleDescription.field = title;
-        titleDescription.headerName = title;
-        titleDescription.width = 70;
-        let copy = { ...titleDescription }; //deepcoy
-        result.push(copy);
-      }
-
-      setColumnsTitles(result);
-      
-      console.log("col2 : \n",columnsTitles)
-    }
-  */
 
   return (
     <div>
